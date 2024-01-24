0b81e6c2b662307e6376c249a5cabd1e4113e772
Nathan-Rubio <nathan.rubio1908@gmail.com>
2024-01-24 09:56:10 -0300

    dinamização do carrossel de imagens

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { paisesVistos } from "./paises-vistos.js";
+
 // FUNÇÕES //
 
 // Adiciona o abrir e fechar do menu de navegação
@@ -23,36 +25,68 @@ function adicionarEventoNavMenu() {
   });
 }
 
+// Adiciona o movimento do carrossel de imagens no grid
+function adicionarEventoImagemButtons() {
+  document.querySelectorAll('.js-anterior-button').forEach((button) => {
+    button.addEventListener('click', () => {
+      const id = button.id;
+      const wrapper = document.getElementById(`wrapper-${id}`);
+      const screenWidth = window.innerWidth;
+  
+      wrapper.scrollTo({
+        left: wrapper.scrollLeft - screenWidth,
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  });
+  
+  document.querySelectorAll('.js-proximo-button').forEach((button) => {
+    button.addEventListener('click', () => {
+      const id = button.id;
+      const wrapper = document.getElementById(`wrapper-${id}`);
+      const screenWidth = window.innerWidth;
+  
+      wrapper.scrollTo({
+        left: wrapper.scrollLeft + screenWidth,
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  });
+}
+
+// Cria o HTML do Grid
+function criarGridHTML() {
+  const paises = paisesVistos;
+
+  let gridHTML = [];
+
+  paises.forEach((pais) => {
+    gridHTML += `
+      <div class="container-pais">
+        <div id="wrapper-${pais.id}" class="wrapper-imagens">
+          <a href="china.html"><img src="${pais.imagem_turismo}"></a>
+          <img src="${pais.imagem_negocios}">
+          <img src="${pais.imagem_estudos}">
+
+          <div class="imagens-buttons">
+            <i id="${pais.id}" class="fa-solid fa-less-than anterior-button js-anterior-button"></i>
+            <i id="${pais.id}" class="fa-solid fa-greater-than proximo-button js-proximo-button"></i>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  document.querySelector('.js-grid').innerHTML = gridHTML;
+}
 
 ///////////////////////////////////////////////////////////////
 
 
 // SCRIPT DA PÁGINA //
 
+criarGridHTML();
 adicionarEventoNavMenu();
-
-document.querySelectorAll('.js-anterior-button').forEach((button) => {
-  button.addEventListener('click', () => {
-    const wrapper = document.getElementById('wrapper');
-    const screenWidth = window.innerWidth;
-
-    wrapper.scrollTo({
-      left: wrapper.scrollLeft - screenWidth,
-      top: 0,
-      behavior: "smooth"
-    });
-  });
-});
-
-document.querySelectorAll('.js-proximo-button').forEach((button) => {
-  button.addEventListener('click', () => {
-    const wrapper = document.getElementById('wrapper');
-    const screenWidth = window.innerWidth;
-
-    wrapper.scrollTo({
-      left: wrapper.scrollLeft + screenWidth,
-      top: 0,
-      behavior: "smooth"
-    });
-  });
-});
+adicionarEventoImagemButtons();
---
+++ paises-vistos.js
@@ -0,0 +1,23 @@
+export const paisesVistos = [
+  {
+    id: "china",
+    nome: "China",
+    imagem_turismo: "imagens/china_turismo.png",
+    imagem_negocios: "imagens/china_negocios.png",
+    imagem_estudos: "imagens/china_estudos.png"
+  },
+  {
+    id: "eua",
+    nome: "Estados Unidos",
+    imagem_turismo: "imagens/eua_turismo.png",
+    imagem_negocios: "imagens/eua_negocios.png",
+    imagem_estudos: "imagens/eua_estudos.png"
+  },
+  {
+    id: "canada",
+    nome: "Canada",
+    imagem_turismo: "imagens/canada_turismo.png",
+    imagem_negocios: "imagens/canada_negocios.png",
+    imagem_estudos: "imagens/canada_estudos.png"
+  }
+];
